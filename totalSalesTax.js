2e3d8ee6b40fd4f126bc243ab54e3c2a7c2c9f7d
tranht86 <tran_ht@hotmail.com>
2017-04-13 13:06:52 +0000

    Test commit

--- totalSalesTax.js
+++ totalSalesTax.js
@@ -22,35 +22,18 @@ var companySalesData = [
   }
 ];
 
+console.log(companySalesData.sales)
 
-
-// function calculateSalesTax(salesData, taxRates) {
-//   // Implement your code here
-//   var summed = 0
-//   var bomsales = companySalesData[1].sales
-//   for (var i = bomsales.length; !!i--;){
-//     summed += bomsales[i];
-//   }
-//   return summed
-// }
-
-function calculateSalesTax(salesData, taxRates) {
-  var totalSales = 0
-  var totalTaxes = 0
-  var bomSales = companySalesData[1].sales
-  var telSales = companySalesData[0].sales
-  var telSales2 = companySalesData[2].sales
-  var bomTotal = 0
-  var telTotal = 0
-
-  if (companySalesData.name = "Bombardier") {
-    for (var i = bomSales.length; !!i--;) {
-      bomTotal += bomSales[i];
-    }
-  } return bomTotal
+function calculateTax(sales, taxRate) {
+  // var comSales =
+  for (i of companySalesData) {
+    if (companySalesData.name === "Telus")
+      console.log(companySalesData)
+  }
+  return companySalesData[sales]
 
 }
-console.log(calculateSalesTax())
+console.log(calculateTax())
 //var results = calculateSalesTax(companySalesData, salesTaxRates);
 
 /* Expected Results:
